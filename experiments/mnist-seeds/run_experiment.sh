86e4f37e966182b5668edaec4e65c53d05639908
Przemyslaw Horban <p.horban@invinets.com>
2017-02-26 11:03:59 +0100

    exp_inting

--- experiments/mnist-seeds/run_experiment.sh
+++ experiments/mnist-seeds/run_experiment.sh
@@ -2,3 +2,10 @@ for ((i=0; i < 4; i++))
 do
     ./run_series.sh $i 20 4 > series_${i}_result.txt &
 done
+
+trap 'echo "If you terminate this, you will leave a mess."' INT
+trap 'echo "If you terminate this, you will leave a mess."' TERM
+
+echo Press any key to terminate the experiment
+read line
+kill -INT $(jobs -p)
