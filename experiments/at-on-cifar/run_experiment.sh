c921ed104f646445bf71d4c4ac1506e2209f1371
Przemyslaw Horban <p.horban@invinets.com>
2017-02-27 09:58:42 +0100

    WISDM experiment

---
+++ experiments/at-on-cifar/run_experiment.sh
@@ -0,0 +1,11 @@
+export THEANO_FLAGS='floatX=float32,device=gpu0,lib.cnmem=0.9'
+export PYTHONPATH=../:$PYTHONPATH
+
+for COUNT in 100 300
+do
+    for SF in 1 2 4 8 16 32
+    do
+        python at_on_cifar.py --count=$COUNT --net_scale_factor=%SF
+    done
+done
+
