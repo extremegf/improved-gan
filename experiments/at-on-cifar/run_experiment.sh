24bf2dab4f88862debb6598a91d840df62fb40ee
Przemyslaw Horban <p.horban@invinets.com>
2017-02-27 10:04:18 +0100

    no message

--- experiments/at-on-cifar/run_experiment.sh
+++ experiments/at-on-cifar/run_experiment.sh
@@ -5,7 +5,8 @@ for COUNT in 100 300
 do
     for SF in 1 2 4 8 16 32
     do
-        python at_on_cifar.py --count=$COUNT --net_scale_factor=%SF
+        python at_on_cifar.py --count=$COUNT --net_scale_factor=$SF \
+          | tee log_count_${COUNT}_sf_${SF}.txt
     done
 done
 
