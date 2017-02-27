6f747d1cf892d9054af28003fbb8a5bd95b2bc6f
Przemyslaw Horban <p.horban@invinets.com>
2017-02-27 10:06:11 +0100

    no message

--- experiments/at-on-cifar/at_on_cifar.py
+++ experiments/at-on-cifar/at_on_cifar.py
@@ -17,7 +17,7 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('--seed', default=1)
 parser.add_argument('--seed_data', default=1)
-parser.add_argument('--count', default=300) # todo: K2 100 300
+parser.add_argument('--count', type=int, default=300) # todo: K2 100 300
 parser.add_argument('--batch_size', default=100)
 parser.add_argument('--unlabeled_weight', type=float, default=1.)
 parser.add_argument('--learning_rate', type=float, default=0.0003)
